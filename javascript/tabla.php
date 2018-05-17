2e92a7c919d6a1f5efdf29fb6211829dbcb626c1
Jonathan Ulises Delgadillo Aguila <36493132+llegoelbigote@users.noreply.github.com>
2018-05-17 16:25:22 -0500

    Update tabla.php
    Quite la linea 5 que contenia el asterisco, para poner la nueva linea para asi ser mas especifico con  los campos que queremos y quite lo de contraseña.

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,7 +2,7 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]">X</button>');
+ $cols = array('id','nombre','fecha','Acciones'=>'button class="borrar" data="@[0]">X>/button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
  
