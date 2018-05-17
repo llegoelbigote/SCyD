f154d0cd0542a29c5d3991ea35f02e0d5d5eff32
Jonathan Ulises Delgadillo Aguila <36493132+llegoelbigote@users.noreply.github.com>
2018-05-17 16:34:04 -0500

    Update tabla.php
    Se guarda primero el icono en la carpeta javacript, posteriormente quitar la X y incorporarle <img scr="bigote.ico" /> para asi pueda llamar el icono

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,7 +2,7 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('id','nombre','fecha','Acciones'=>'button class="borrar" data="@[0]">X>/button>');
+ $cols = array('id','nombre','fecha','Acciones'=>'button class="borrar" data="@[0]"><img scr="bigote.ico" >/button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
  
